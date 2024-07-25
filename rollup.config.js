f60e4cc67287f2b1bbc4235749b2d444393a4fba
eunniverse <eunisss98@gmail.com>
2024-07-25 22:47:01 +0900

    jsx 빌드 오류 수정
    - extensions 추가
    - 빌드 방식 변경 (commonJS)

--- rollup.config.js
+++ rollup.config.js
@@ -3,24 +3,28 @@ import commonjs from '@rollup/plugin-commonjs';
 import babel from '@rollup/plugin-babel';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import { terser } from 'rollup-plugin-terser';
+import json from '@rollup/plugin-json';
 
 export default {
     input: './index.js',
     output:{
             file: 'dist/index.esm.js',
-            format: 'es',
+            format: 'cjs',
             sourcemap: true
     },
     external: ['react'],
     plugins: [
         peerDepsExternal(),
-        resolve(),
+        resolve({
+            extensions: ['.js', '.jsx'], // 파일 확장자 설정
+        }),
         babel({
             exclude: 'node_modules/**',
             babelHelpers: 'bundled',
             presets: ['@babel/preset-env', '@babel/preset-react']
         }),
         commonjs(),
-        terser()
+        terser(),
+        json()
     ]
 };
